907ae0e4d6c910fb12cfd3ea22630d8928076adf
Crescent117 <127200596+Crescent117@users.noreply.github.com>
2023-05-03 18:18:59 +0900

    Delete amusement directory

--- amusement/src/main/java/com/exciting/index/service/PromotionService.java
+++
@@ -1,9 +0,0 @@
-package com.exciting.index.service;
-
-import java.util.List;
-
-import com.exciting.dto.PromotionDTO;
-
-public interface PromotionService {
-	public List<PromotionDTO> selectList();
-}
