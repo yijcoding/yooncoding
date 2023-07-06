f70c8cde5defc415ca32d959c60cc79f53f2aece
kimdongouk <61115783+kimdongouk@users.noreply.github.com>
2023-07-06 17:07:57 +0900

    삭제

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>)
