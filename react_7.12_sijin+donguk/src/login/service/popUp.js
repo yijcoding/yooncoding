058d56bd8aeb47c976a9efbd0ee62eaeee0fd6f2
sijin <dkstlwlss@naver.com>
2023-07-20 18:06:32 +0900

    230720 sijin+donguk remove

--- react_7.12_sijin+donguk/src/login/service/popUp.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export function openPopup(url) {
-  const popupWindow = window.open(url, "_blank", "width=600,height=880");
-  if (popupWindow) {
-    popupWindow.focus();
-  }
-}
--- react_7.12_sijin+donguk/src/login/views/githubLogin.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { searchQuery } from "../service/searchQuery";
-
-export function GithubLogin() {
-  const access_token = searchQuery("token");
-  localStorage.setItem("ACCESS_TOKEN", access_token);
-  window.location.href = "/main";
-}
